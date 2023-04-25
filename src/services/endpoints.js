21d750478f5b8f8c4fbe54fa470fa145293b0db5
Eduardo Serrano <eduardoserranojaime@gmail.com>
2023-04-25 10:06:43 -0600

    remove api from env variables

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -1,8 +1,8 @@
-const API = process.env.NEXT_PUBLIC_API_URL;
+// const API = process.env.NEXT_PUBLIC_API_URL;
 
 const endPoints = {
   registers: {
-    main: `${API}/registers`,
+    main: `/api/registers`,
   },
 };
 
