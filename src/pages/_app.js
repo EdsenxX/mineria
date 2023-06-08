c88dccc8e2b8985be0cfa33bd107c327d45408f5
Eduardo Serrano <eduardoserranojaime@gmail.com>
2023-06-07 23:06:42 -0600

    add headers and loader in main page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@styles/globals.css';
+// Dependencies
+import Head from "next/head";
+// Styles
+import "@styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Eduardo Serrano | Mineria de datos</title>
+        <meta name="description" content="09 Conexión SdDD BdDD | Mineria de datos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
 // Dependencies
 import Head from "next/head";
-import Swal from 'sweetalert2';
-import { useState, useEffect } from 'react'
+import Swal from "sweetalert2";
+import { useState, useEffect } from "react";
 import csvtojson from "csvtojson";
 import { useRouter } from "next/router";
 // Components
 import { Button, Loading, Progress } from "@nextui-org/react";
 import InputFile from "@components/InputFile";
+import Loader from "@components/Loader";
 // Services
 import { createRegisters, getRegisters } from "@services/registers";
 // Utils
@@ -14,50 +15,51 @@ import typeFiles from "@utils/typeFiles.constants";
 import { csvFile } from "@utils/defaultTypeFileError";
 
 export default function Home() {
-
   const router = useRouter();
 
   const { csv } = typeFiles;
 
   const [loading, setLoading] = useState(false);
-  const [dynamicText, setDynamicText] = useState("Número maximo de registros: 100,000");
+  const [dynamicText, setDynamicText] = useState(
+    "Número maximo de registros: 100,000"
+  );
   const [files, setFiles] = useState([]);
   const [progress, setProgress] = useState(0);
+  const [initialLoading, setInitialLoading] = useState(true);
 
   const handleImportFile = async (e) => {
-
     e.preventDefault();
-    if(loading) return;
+    if (loading) return;
     setLoading(true);
-    const file = document.getElementById('files').files[0];
-    if(!file) {
+    const file = document.getElementById("files").files[0];
+    if (!file) {
       Swal.fire(
-        'Archivo no valido',
-        'Por favor selecciona un archivo',
-        'warning'
-      )
+        "Archivo no valido",
+        "Por favor selecciona un archivo",
+        "warning"
+      );
       setLoading(false);
       return;
     }
     // validate file type are csv
     if (file.type !== "text/csv") {
       Swal.fire(
-        'Archivo no valido',
-        'Solo se aceptan archivos .csv, por favor intenta de nuevo',
-        'warning'
-      )
+        "Archivo no valido",
+        "Solo se aceptan archivos .csv, por favor intenta de nuevo",
+        "warning"
+      );
       setLoading(false);
     }
     // get file content
     // get 10 first registers
-    setDynamicText('Dividiendo el archivo en chunks de 20000....')
-    const fileContent = await file.text()
-    const fileTransformed = await csvtojson().fromString(fileContent)
+    setDynamicText("Dividiendo el archivo en chunks de 20000....");
+    const fileContent = await file.text();
+    const fileTransformed = await csvtojson().fromString(fileContent);
     // separate registers in chunks of 50000
     const chunkSize = 20000;
     const chunks = [];
     for (let i = 0; i < fileTransformed.length; i += chunkSize) {
-      if(chunks.length >= 5) {
+      if (chunks.length >= 5) {
         break;
       }
       chunks.push(fileTransformed.slice(i, i + chunkSize));
@@ -65,28 +67,32 @@ export default function Home() {
 
     // send chunks to server
     for (let i = 0; i < chunks.length; i++) {
-      setDynamicText(`Enviando chunk ${i + 1} de ${chunks.length} al servidor...`)
+      setDynamicText(
+        `Enviando chunk ${i + 1} de ${chunks.length} al servidor...`
+      );
       const chunk = chunks[i];
       await createRegisters(chunk);
       setProgress(((i + 1) / chunks.length) * 100);
     }
-    setLoading(false)
+    setLoading(false);
 
-    setDynamicText('Archivo importado correctamente')
+    setDynamicText("Archivo importado correctamente");
     Swal.fire(
-      'Archivo importado',
-      'El archivo se ha importado correctamente',
-      'success'
-    )
-    router.push('/main')
+      "Archivo importado",
+      "El archivo se ha importado correctamente",
+      "success"
+    );
+    router.push("/main");
   };
 
   const handleGetRegisters = async () => {
+    setInitialLoading(true);
     const registers = await getRegisters();
-    if(registers.length) {
-      router.push('/main')
+    if (registers.length) {
+      router.push("/main");
     }
-  }
+    setInitialLoading(false);
+  };
 
   useEffect(() => {
     handleGetRegisters();
@@ -112,29 +118,52 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex justify-center items-center min-w-screen min-h-screen">
-        <form onSubmit={handleImportFile} className="bg-white shadow-xl p-4 rounded-xl text-black flex flex-col items-center gap-2 min-w-[400px]">
-          <p className="text-xl font-bold mb-5">Importa el archivo de tu dataset</p>
-          <InputFile
-            id="files"
-            onChange={handleAddFiles}
-            onRemoveFile={handleRemoveFiles}
-            files={files}
-            allowedTypes={[csv]}
-            defaultTypeFileError={csvFile}
-          />
-          <Button type="submit" icon={loading && <Loading type="spinner" color="currentColor" size="sm" />}> Importar </Button>
+      {initialLoading ? (
+        <div className="w-screen h-screen flex justify-center items-center">
+          <Loader />
+        </div>
+      ) : (
+        <main className="flex justify-center items-center min-w-screen min-h-screen">
+          <form
+            onSubmit={handleImportFile}
+            className="bg-white shadow-xl p-4 rounded-xl text-black flex flex-col items-center gap-2 min-w-[400px]"
+          >
+            <p className="text-xl font-bold mb-5">
+              Importa el archivo de tu dataset
+            </p>
+            <InputFile
+              id="files"
+              onChange={handleAddFiles}
+              onRemoveFile={handleRemoveFiles}
+              files={files}
+              allowedTypes={[csv]}
+              defaultTypeFileError={csvFile}
+            />
+            <Button
+              type="submit"
+              icon={
+                loading && (
+                  <Loading type="spinner" color="currentColor" size="sm" />
+                )
+              }
+            >
+              {" "}
+              Importar{" "}
+            </Button>
             <>
               {loading && (
                 <>
                   <Progress color="primary" size="md" value={progress} />
-                  <p className="text-sm">Por favor espere, este proceso puede tardar varios minutos</p>
+                  <p className="text-sm">
+                    Por favor espere, este proceso puede tardar varios minutos
+                  </p>
                 </>
               )}
               <p className="text-sm">{dynamicText}</p>
             </>
-        </form>
-      </main>
+          </form>
+        </main>
+      )}
     </>
   );
 }
